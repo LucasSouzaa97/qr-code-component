5bf31595f0907ef1b85c54b3c1802ed2cff527aa
Lucas de Souza <111311202+LucasSouzaa97@users.noreply.github.com>
2023-11-25 21:35:35 -0300

    Add TailwindCSS configuration and corresponding dependencies
    This commit adds a configuration for TailwindCSS. Two new files, postcss.config.js and tailwind.config.js, have been added to set up TailwindCSS with custom themes and plugins. Also, dependencies related to TailwindCSS and PostCSS have been added to package-lock.json. This will enable the project to utilize TailwindCSS for styling.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        screens: {
+            desktop: '1440px',
+        },
+        colors: {
+            white: '#FFFFFF',
+            'light-gray': '#D5E1EF',
+            'grayish-blue': '#7D889E',
+            'dark-yellowish-green': '#6f7f66',
+            'dark-blue': '#1D2D44',
+        },
+        fontFamily: {
+            outfit: ['Outfit', 'sans-serif'],
+        },
+        fontSize: {
+            paragraph: '15px',
+        },
+        extend: {},
+    },
+    plugins: ['prettier-plugin-tailwindcss'],
+};
